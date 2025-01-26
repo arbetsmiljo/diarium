88655980b198daac3029c0241b8b6bae3f930bf0
Henry Catalini Smith <henry@catalinismith.com>
2025-01-26 21:25:11 +0100

    Get basic scraping working

--- bin.ts
+++ bin.ts
@@ -4,20 +4,22 @@ import { Command } from "commander";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { readFileSync } from "fs";
-import { version } from "./version";
+import { document } from "./document";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const pkg = JSON.parse(readFileSync(`${__dirname}/../package.json`, "utf8"));
 
 const program = new Command();
-program.name(pkg.name).description("Starter Node project").version(pkg.name);
+program.name(pkg.name).description(pkg.description).version(pkg.name);
 
 program
-  .command("version")
-  .description("Show version")
-  .action(async () => {
-    process.stdout.write(`${await version()}\n`);
+  .command("document")
+  .description("Fetch document metadata")
+  .argument("<id>", "Document ID")
+  .action(async (id) => {
+    const data = await document(id);
+    process.stdout.write(`${JSON.stringify(data, null, 2)}\n`);
   });
 
 program.parse(process.argv);
