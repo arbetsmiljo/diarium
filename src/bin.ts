969b2adfa0840cb95015be823239b3ae97dc5a7f
Henry Catalini Smith <henry@catalinismith.se>
2025-01-27 19:46:02 +0100

    More renaming mostly

--- src/bin.ts
+++ src/bin.ts
@@ -4,16 +4,23 @@ import { Command } from "commander";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { readFileSync } from "fs";
+import { createDatabase } from "@/database";
 import { fetchDocument } from "@/document";
-import { initSchema } from "@/schema";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const pkg = JSON.parse(readFileSync(`${__dirname}/../package.json`, "utf8"));
 
 const program = new Command();
-console.log(pkg);
 program.name(pkg.name).description(pkg.description).version(pkg.name);
+
+program
+  .command("createDatabase")
+  .description("Create a new blank database")
+  .option("-f, --filename <filename>", "database filename", "db.sqlite")
+  .action(async ({ filename }) => {
+    await createDatabase(filename);
+  });
 
 program
   .command("fetchDocument")
@@ -23,13 +30,4 @@ program
     const data = await fetchDocument(id);
     process.stdout.write(`${JSON.stringify(data, null, 2)}\n`);
   });
-
-program
-  .command("initSchema")
-  .description("Create a new blank database")
-  .option("-f, --filename <filename>", "database filename", "db.sqlite")
-  .action(async ({ filename }) => {
-    await initSchema(filename);
-  });
-
 program.parse(process.argv);
