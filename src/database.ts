4d645a1c22088f0323f2cec4c675d8c88ce4c0c2
Henry Catalini Smith <henry@catalinismith.com>
2025-01-27 19:46:02 +0100

    More renaming mostly

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,13 @@
 import sqlite3 from "sqlite3";
+import fs from "fs";
 
 /**
  * Creates a new blank database
  */
-export async function initSchema(filename: string): Promise<void> {
+export async function createDatabase(filename: string): Promise<void> {
+  if (fs.existsSync(filename)) {
+    throw new Error(`Database file already exists: ${filename}`);
+  }
   const database = new sqlite3.Database(filename);
   database.exec(`
     CREATE TABLE documents (
