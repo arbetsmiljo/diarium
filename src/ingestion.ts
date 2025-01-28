c4fa24f0fc8f2d05e91a97e110fd860e22de8f9a
Henry Catalini Smith <henry@catalinismith.com>
2025-01-28 21:14:10 +0100

    Add throttle and output

--- src/ingestion.ts
+++ src/ingestion.ts
@@ -1,21 +1,34 @@
 import { fetchDiariumDocument } from "./document";
 import { fetchDiariumPage } from "./pagination";
+import ora from "ora";
 
-export async function ingestDiariumDay(date: string) {
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export async function ingestDiariumDay(date: string, ms = 1000) {
   let pageNumber = 1;
+  let pageSpinner = ora(` Page ${pageNumber}`).start();
   let page = await fetchDiariumPage(date, pageNumber);
+  pageSpinner.succeed(
+    ` Page ${pageNumber}: ${page.documents.length * (pageNumber - 1)} - ${
+      page.documents.length * (pageNumber - 1) + page.documents.length
+    } of ${page.hitCount}`,
+  );
   const total = page.hitCount;
   let remaining = total - page.documents.length;
 
   while (remaining > 0 && page.documents.length > 0) {
     pageNumber += 1;
     page = await fetchDiariumPage(date, pageNumber);
-    const documents = await Promise.all(
-      page.documents.map(async (document) => {
-        return await fetchDiariumDocument(document.id);
-      }),
-    );
-    console.log(documents.map((d) => d.companyCode));
+
+    for (let i = 0; i < page.documents.length; i++) {
+      const { id } = page.documents[i];
+      const documentSpinner = ora(` ${id}`).start();
+      await delay(ms);
+      const document = await fetchDiariumDocument(id);
+      documentSpinner.succeed(
+        ` ${document.id}: ${document.documentType} ${document.companyName ? `(${document.companyName})` : ""}`,
+      );
+    }
 
     remaining = 0;
   }
